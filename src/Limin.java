acd5e3a4f6eafbcc6bc4831225c988d68b4ee359
Вероника <veronika@192.168.1.103>
2022-05-04 14:01:35 +0300

    Initial commit

---
+++ src/Limin.java
@@ -0,0 +1,14 @@
+public class Limin {
+    public static void main(String[] args) {
+            int liVoin = 13;
+            int liLyhnic = 24;
+            int liVerhnic = 46;
+            int minVoin = 9;
+            int minLyhnic = 35;
+            int minVerhnic = 12;
+            int li = (liVoin + liLyhnic + liVerhnic) * 860;
+            int min = (int) ((minVoin + minLyhnic + minVerhnic) * (860 * 1.5));
+            System.out.println("armiia Li = " + li);
+            System.out.println("armiia Min = " + min);
+    }
+}
